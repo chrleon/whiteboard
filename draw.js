d56cd503976e61424d94449ecaae141f50cc9ed7
Christian Leon Christensen <chrleon@gmail.com>
2023-06-17 10:58:09 +0200

    Fill the entire browser

--- draw.js
+++ draw.js
@@ -1,79 +1,77 @@
-// Get canvas element and context
-let canvas = document.querySelector('#draw-area');
+let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
-
-// Default drawing color
-let color = 'black';
-
-// State variables
-let drawing = false;   // Whether we're currently drawing
-let panning = false;   // Whether we're currently panning
-let panStartX = 0;     // X-coordinate where the current pan started
-let panStartY = 0;     // Y-coordinate where the current pan started
-let offsetX = 0;       // Total X-panning offset
-let offsetY = 0;       // Total Y-panning offset
+let drawing = false;
+let panning = false;
 let justPanned = false;
-
-// Points that have been drawn
+let panStartX, panStartY;
+let offsetX = 0, offsetY = 0;
 let line = [];
+let color = '#000000';
+
+// Get toolbar buttons
+let drawBtn = document.getElementById('draw-btn');
+let eraseBtn = document.getElementById('erase-btn');
+
+// Set up listeners for toolbar buttons
+drawBtn.addEventListener('click', function() {
+    color = '#000000'; // Set color to black for drawing
+});
+
+eraseBtn.addEventListener('click', function() {
+    color = '#FFFFFF'; // Set color to white for erasing
+});
 
 canvas.addEventListener('mousedown', function(e) {
-  if (e.button === 2) {  // right click
+  if (e.button === 2) { // Right click
       drawing = false;
       panning = true;
       panStartX = e.clientX;
       panStartY = e.clientY;
-  } else {  // left click
+  } else { // Left click
       drawing = true;
+      ctx.beginPath();  // Start a new path
+      justPanned = false;
       let x = e.clientX - canvas.offsetLeft;
       let y = e.clientY - canvas.offsetTop;
-      if (justPanned) {
-          ctx.beginPath();
-          justPanned = false;
-      }
       line.push({x: x - offsetX, y: y - offsetY, color: color, move: true});
   }
 });
 
-canvas.addEventListener('mouseup', function(e) {
-  if (e.button === 2) {  // right click
-      panning = false;
-      justPanned = true;
-  } else { // left click
-      drawing = false;
-  }
-});
 
 
 canvas.addEventListener('mousemove', function(e) {
-  if (drawing) {
-      draw(e);
-  } else if (panning) {
-      offsetX += e.clientX - panStartX;
-      offsetY += e.clientY - panStartY;
-      panStartX = e.clientX;
-      panStartY = e.clientY;
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      drawAll();
+    if (drawing) {
+        draw(e);
+    } else if (panning) {
+        offsetX += e.clientX - panStartX;
+        offsetY += e.clientY - panStartY;
+        panStartX = e.clientX;
+        panStartY = e.clientY;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawAll();
+    }
+});
+
+canvas.addEventListener('mouseup', function(e) {
+  if (e.button === 2) { // Right click
+      panning = false;
+      justPanned = true;
+  } else { // Left click
+      drawing = false;
+      ctx.beginPath();  // End the path
   }
 });
 
-// Prevent the context menu from showing up on right click
+
 canvas.addEventListener('contextmenu', function(e) {
     e.preventDefault();
 });
 
-// On pen click, switch to drawing mode
-document.getElementById('pen').addEventListener('click', function() {
-    color = 'black';
+// Adjust canvas size when window size changes
+window.addEventListener('resize', function() {
+    resizeCanvas();
 });
 
-// On eraser click, switch to erasing mode
-document.getElementById('eraser').addEventListener('click', function() {
-    color = 'white';
-});
-
-// Draw a point at the current mouse position
 function draw(e) {
   if (!drawing) return;
   let x = e.clientX - canvas.offsetLeft;
@@ -83,24 +81,33 @@ function draw(e) {
   ctx.strokeStyle = color;
   ctx.lineTo(x, y);
   ctx.stroke();
-  ctx.beginPath();
+  // removed ctx.beginPath();
   ctx.moveTo(x, y);
   line.push({x: x - offsetX, y: y - offsetY, color: color});
 }
 
+
+
 function drawAll() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i < line.length; i++) {
-      let point = line[i];
-      ctx.lineWidth = 5;
-      ctx.lineCap = 'round';
-      ctx.strokeStyle = point.color;
-      if (point.move) {
-          ctx.beginPath();
-          ctx.moveTo(point.x + offsetX, point.y + offsetY);
-      } else {
-          ctx.lineTo(point.x + offsetX, point.y + offsetY);
-          ctx.stroke();
-      }
-  }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < line.length; i++) {
+        let point = line[i];
+        ctx.lineWidth = 5;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = point.color;
+        if (point.move) {
+            ctx.beginPath();
+            ctx.moveTo(point.x + offsetX, point.y + offsetY);
+        } else {
+            ctx.lineTo(point.x + offsetX, point.y + offsetY);
+            ctx.stroke();
+        }
+    }
 }
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight - 30; // Subtract the height of the toolbar
+}
+
+resizeCanvas();
