73efd8b9d6bf93929a51e650d819a756bfb45d1f
Christian Leon Christensen <chrleon@gmail.com>
2023-06-17 10:48:51 +0200

    initial infinite canvas

--- draw.js
+++ draw.js
@@ -12,46 +12,50 @@ let panStartX = 0;     // X-coordinate where the current pan started
 let panStartY = 0;     // Y-coordinate where the current pan started
 let offsetX = 0;       // Total X-panning offset
 let offsetY = 0;       // Total Y-panning offset
+let justPanned = false;
 
 // Points that have been drawn
 let line = [];
 
-// On mousedown, start drawing or panning
 canvas.addEventListener('mousedown', function(e) {
-    if (e.button === 2) {  // right click
-        drawing = false;
-        panning = true;
-        panStartX = e.clientX;
-        panStartY = e.clientY;
-    } else {  // left click
-        drawing = true;
-        draw(e);
-    }
+  if (e.button === 2) {  // right click
+      drawing = false;
+      panning = true;
+      panStartX = e.clientX;
+      panStartY = e.clientY;
+  } else {  // left click
+      drawing = true;
+      let x = e.clientX - canvas.offsetLeft;
+      let y = e.clientY - canvas.offsetTop;
+      if (justPanned) {
+          ctx.beginPath();
+          justPanned = false;
+      }
+      line.push({x: x - offsetX, y: y - offsetY, color: color, move: true});
+  }
 });
 
-// On mouseup, stop drawing or panning
 canvas.addEventListener('mouseup', function(e) {
-    if (e.button === 2) {  // right click
-        panning = false;
-    } else {  // left click
-        drawing = false;
-        ctx.beginPath();
-    }
+  if (e.button === 2) {  // right click
+      panning = false;
+      justPanned = true;
+  } else { // left click
+      drawing = false;
+  }
 });
 
-// On mousemove, draw or pan
+
 canvas.addEventListener('mousemove', function(e) {
-    if (panning) {
-        drawing = false;
-        offsetX += e.clientX - panStartX;
-        offsetY += e.clientY - panStartY;
-        panStartX = e.clientX;
-        panStartY = e.clientY;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawAll();
-    } else if (drawing) {
-        draw(e);
-    }
+  if (drawing) {
+      draw(e);
+  } else if (panning) {
+      offsetX += e.clientX - panStartX;
+      offsetY += e.clientY - panStartY;
+      panStartX = e.clientX;
+      panStartY = e.clientY;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      drawAll();
+  }
 });
 
 // Prevent the context menu from showing up on right click
@@ -71,28 +75,32 @@ document.getElementById('eraser').addEventListener('click', function() {
 
 // Draw a point at the current mouse position
 function draw(e) {
-    if (!drawing) return;
-    let x = e.clientX - canvas.offsetLeft - offsetX;
-    let y = e.clientY - canvas.offsetTop - offsetY;
-    ctx.lineWidth = 5;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = color;
-    line.push({x, y, color});
-    ctx.lineTo(x, y);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(x, y);
+  if (!drawing) return;
+  let x = e.clientX - canvas.offsetLeft;
+  let y = e.clientY - canvas.offsetTop;
+  ctx.lineWidth = 5;
+  ctx.lineCap = 'round';
+  ctx.strokeStyle = color;
+  ctx.lineTo(x, y);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  line.push({x: x - offsetX, y: y - offsetY, color: color});
 }
 
-// Redraw all points that have been drawn
 function drawAll() {
-    for (let point of line) {
-        ctx.lineWidth = 5;
-        ctx.lineCap = 'round';
-        ctx.strokeStyle = point.color;
-        ctx.lineTo(point.x + offsetX, point.y + offsetY);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(point.x + offsetX, point.y + offsetY);
-    }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < line.length; i++) {
+      let point = line[i];
+      ctx.lineWidth = 5;
+      ctx.lineCap = 'round';
+      ctx.strokeStyle = point.color;
+      if (point.move) {
+          ctx.beginPath();
+          ctx.moveTo(point.x + offsetX, point.y + offsetY);
+      } else {
+          ctx.lineTo(point.x + offsetX, point.y + offsetY);
+          ctx.stroke();
+      }
+  }
 }
